a3c18aa6bb44e40a1e006f0b746c66efee32dc6e
QueenKimm <gudarzikimia@gmail.com>
2025-02-01 17:49:56 -0500

    User service

--- src/main/java/com/example/CryptoExchange/UserService.java
+++ src/main/java/com/example/CryptoExchange/UserService.java
@@ -1,28 +1,28 @@
 package com.example.CryptoExchange;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    private final passwordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.passwordEncoder = new BCryptEncoder();
+        this.passwordEncoder = passwordEncoder;
     }
-    public User registerUser(String username, String email ,String password){
 
-        if(userRepository.findByUsername(username).isPresent()){
+    public User registerUser(String username, String email, String password) {
+        if (userRepository.findByUsername(username).isPresent()) {
             throw new RuntimeException("Username is already taken");
         }
-        if(userRepository.findByEmail(email).isPresent()){
+        if (userRepository.findByEmail(email).isPresent()) {
             throw new RuntimeException("Email is already registered");
         }
+
+
         String encryptedPassword = passwordEncoder.encode(password);
 
         User newUser = new User(username, email, encryptedPassword);
