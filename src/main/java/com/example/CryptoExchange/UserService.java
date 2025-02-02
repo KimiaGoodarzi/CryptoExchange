b46459752ee0791dd7d18695a53c2ff21027c9bc
QueenKimm <gudarzikimia@gmail.com>
2025-02-02 17:34:19 -0500

    User service

--- src/main/java/com/example/CryptoExchange/UserService.java
+++ src/main/java/com/example/CryptoExchange/UserService.java
@@ -9,23 +9,23 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User registerUser(String username, String email, String password) {
-        if (userRepository.findByUsername(username).isPresent()) {
-            throw new RuntimeException("Username is already taken");
+    public User registerUser(String username, String email, String password){
+        if(userRepository.findByUsername(username).isPresent()){
+            throw new RuntimeException("Username is already taken.");
         }
-        if (userRepository.findByEmail(email).isPresent()) {
-            throw new RuntimeException("Email is already registered");
+        if(userRepository.findByEmail(email).isPresent()){
+            throw new RuntimeException("This email address is already registered.");
         }
 
-
-        String encryptedPassword = passwordEncoder.encode(password);
-
+        String encryptedPassword  = passwordEncoder.encode(password);
         User newUser = new User(username, email, encryptedPassword);
         return userRepository.save(newUser);
     }
+
+
 }
