7b30ed284642c501ac1ed7feed486dfeee3dfd64
QueenKimm <gudarzikimia@gmail.com>
2025-02-03 21:26:39 -0500

    Create JwtUtil to Generate JWT Tokens

---
+++ src/main/java/com/example/CryptoExchange/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.example.CryptoExchange;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.function.Function;
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "KimiaGoodarzi";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 10; //valid for ten hours
+
+
+    public String generateToken(String username){
+
+        return Jwts.builder().setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY).build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean validateToken(String token, String username) {
+        return username.equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+}
